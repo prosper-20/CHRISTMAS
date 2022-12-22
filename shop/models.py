52740de859743afde6bf86b55fecd21565dd809f
prosper <edwardprosper001@gmail.com>
2022-12-22 22:21:45 +0100

    initial

--- shop/models.py
+++ shop/models.py
@@ -3,33 +3,40 @@ from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200,
+                            unique=True)
 
     class Meta:
         ordering = ['name']
         indexes = [
-        models.Index(fields=['name']),
+            models.Index(fields=['name']),
         ]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
-    def get_absolute_url(self):
-        return reverse('product_list_by_category', args=[self.slug])
-
     def __str__(self):
         return self.name
 
+    def get_absolute_url(self):
+        return reverse('product_list_by_category',
+                       args=[self.slug])
+
+
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    category = models.ForeignKey(Category,
+                                 related_name='products',
+                                 on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
-    image = models.ImageField(upload_to='products', blank=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d',
+                              blank=True)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    
+
     class Meta:
         ordering = ['name']
         indexes = [
@@ -38,8 +45,9 @@ class Product(models.Model):
             models.Index(fields=['-created']),
         ]
 
-    def get_absolute_url(self):
-        return reverse('product_detail', args=[self.id, self.slug])
-
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('product_detail',
+                       args=[self.id, self.slug])

0e65b2f84a3254c80f5243665d4e16bdec800284
prosper <edwardprosper001@gmail.com>
2022-12-22 21:42:27 +0100

    initial

--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
 from shop.cart import Cart
-
+from .forms import CartAddProductForm
 
 @require_POST
 def cart_add(request, product_id):
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Category, Product
-from .forms import CartAddProductForm
+from cart.forms import CartAddProductForm
 
 def product_list(request, category_slug=None):
     category = None

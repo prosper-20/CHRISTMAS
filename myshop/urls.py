050b7eea55f8a75a634907df89e1e94ef0b5d020
prosper <edwardprosper001@gmail.com>
2022-12-22 22:25:53 +0100

    initial

--- myshop/urls.py
+++ myshop/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('cart/', include('cart.urls', namespace='cart')),
+    path('orders/', include('orders.urls', namespace='orders')),
 ]
 
 if settings.DEBUG:

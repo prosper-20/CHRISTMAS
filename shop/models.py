f893c96fd69b2ccefce7861e7cf6309f97d8ca32
prosper <edwardprosper001@gmail.com>
2022-12-22 21:04:49 +0100

    initial

--- shop/models.py
+++ shop/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
-    slug = models.SlugField(max_length=200,
-    unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
 
     class Meta:
         ordering = ['name']
@@ -12,6 +12,9 @@ class Category(models.Model):
         ]
         verbose_name = 'category'
         verbose_name_plural = 'categories'
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
     def __str__(self):
         return self.name
@@ -34,6 +37,9 @@ class Product(models.Model):
             models.Index(fields=['name']),
             models.Index(fields=['-created']),
         ]
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
 
     def __str__(self):
         return self.name

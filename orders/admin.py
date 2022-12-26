2986916c1f624bcd54fec7659d2fdaf12bb94c0b
prosper <edwardprosper001@gmail.com>
2022-12-26 20:11:26 +0100

    updated code

--- orders/admin.py
+++ orders/admin.py
@@ -50,3 +50,4 @@ class OrderAdmin(admin.ModelAdmin):
     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
+    actions = [export_to_csv]
--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render, redirect
-from .models import OrderItem
+from .models import OrderItem, Order
 from .forms import OrderCreateForm
 from .tasks import order_created
 from cart.cart import Cart
 from .tasks import order_created
-from django.urls import reverse
+from django.urls import reverse, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
 
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    return render(request,
+    'admin/orders/order/detail.html',
+    {'order': order})
 
 def order_create(request):
     cart = Cart(request)

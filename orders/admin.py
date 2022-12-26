7440dad9f18c10c1b6b4d5e190ca203c7be4c875
prosper <edwardprosper001@gmail.com>
2022-12-26 20:31:57 +0100

    updated code

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,10 @@
-from django.contrib import admin
 import csv
 import datetime
-from .models import Order, OrderItem
-from django.utils.safestring import mark_safe
 from django.http import HttpResponse
-from .models import Order, OrderItem
+from django.contrib import admin
 from django.urls import reverse
+from django.utils.safestring import mark_safe
+from .models import Order, OrderItem
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -30,6 +29,12 @@ def export_to_csv(modeladmin, request, queryset):
     return response
 export_to_csv.short_description = 'Export to CSV'
 
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    raw_id_fields = ['product']
+
+
 def order_payment(obj):
     url = obj.get_stripe_url()
     if obj.stripe_id:
@@ -37,12 +42,6 @@ def order_payment(obj):
         return mark_safe(html)
     return ''
 order_payment.short_description = 'Stripe payment'
-
-
-class OrderItemInline(admin.TabularInline):
-    model = OrderItem
-    raw_id_fields = ['product']
-
 
 def order_detail(obj):
     url = reverse('orders:admin_order_detail', args=[obj.id])
@@ -54,11 +53,13 @@ def order_pdf(obj):
     return mark_safe(f'<a href="{url}">PDF</a>')
 order_pdf.short_description = 'Invoice'
 
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
-    'address', 'postal_code', 'city', 'paid', 'order_payment',
-    'created', 'updated', 'order_detail']
+                    'address', 'postal_code', 'city', 'paid',
+                    order_payment, 'created', 'updated',
+                    order_detail, order_pdf]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]

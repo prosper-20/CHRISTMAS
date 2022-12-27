6e42eb0c646d6c8e1edb9889e6f162619c2aff77
prosper <edwardprosper001@gmail.com>
2022-12-27 21:41:43 +0100

    updates

---
+++ coupons/__init__.py
@@ -0,0 +1,28 @@
+
+
+class Cart:
+    def __init__(self, request):
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+            self.cart = cart
+            self.coupon_id = self.session.get('coupon_id')
+
+    @property
+    def coupon(self):
+        if self.coupon_id:
+            try:
+                return Coupon.objects.get(id=self.coupon_id)
+            except Coupon.DoesNotExist:
+                pass
+                return None
+
+    def get_discount(self):
+        if self.coupon:
+            return (self.coupon.discount / Decimal(100)) \
+            * self.get_total_price()
+        return Decimal(0)
+    
+    def get_total_price_after_discount(self):
+        return self.get_total_price() - self.get_discount()

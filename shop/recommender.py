7d39e1268e4a29b997133997d013067ca9dc54ac
prosper <edwardprosper001@gmail.com>
2022-12-29 19:09:07 +0100

    new

--- shop/recommender.py
+++ shop/recommender.py
@@ -2,8 +2,11 @@ import redis
 from django.conf import settings
 from .models import Product
 
+
 # connect to redis
-r = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+r = redis.Redis(host=settings.REDIS_HOST,
+                port=settings.REDIS_PORT,
+                db=settings.REDIS_DB)
 
 
 class Recommender:
@@ -14,22 +17,22 @@ class Recommender:
         product_ids = [p.id for p in products]
         for product_id in product_ids:
             for with_id in product_ids:
-            # get the other products bought with each product
+                # get the other products bought with each product
                 if product_id != with_id:
-                # increment score for product purchased together
+                    # increment score for product purchased together
                     r.zincrby(self.get_product_key(product_id),
-                    1,
-                    with_id)
+                              1,
+                              with_id)
 
     def suggest_products_for(self, products, max_results=6):
         product_ids = [p.id for p in products]
         if len(products) == 1:
-        # only 1 product
+            # only 1 product
             suggestions = r.zrange(
-            self.get_product_key(product_ids[0]),
-            0, -1, desc=True)[:max_results]
+                             self.get_product_key(product_ids[0]),
+                             0, -1, desc=True)[:max_results]
         else:
-        # generate a temporary key
+            # generate a temporary key
             flat_ids = ''.join([str(id) for id in product_ids])
             tmp_key = f'tmp_{flat_ids}'
             # multiple products, combine scores of all products
@@ -40,15 +43,14 @@ class Recommender:
             r.zrem(tmp_key, *product_ids)
             # get the product ids by their score, descendant sort
             suggestions = r.zrange(tmp_key, 0, -1,
-            desc=True)[:max_results]
+                                   desc=True)[:max_results]
             # remove the temporary key
             r.delete(tmp_key)
-            suggested_products_ids = [int(id) for id in suggestions]
-            # get suggested products and sort by order of appearance
-            suggested_products = list(Product.objects.filter(
-            id__in=suggested_products_ids))
-            suggested_products.sort(key=lambda x: suggested_products_ids.index(x.id))
-            return suggested_products
+        suggested_products_ids = [int(id) for id in suggestions]
+        # get suggested products and sort by order of appearance
+        suggested_products = list(Product.objects.filter(id__in=suggested_products_ids))
+        suggested_products.sort(key=lambda x: suggested_products_ids.index(x.id))
+        return suggested_products
 
     def clear_purchases(self):
         for id in Product.objects.values_list('id', flat=True):
